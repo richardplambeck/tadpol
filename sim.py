f0a7ff168f7e65ed603661df5918f7e58e65d148
Dick Plambeck <plambeck@astro.berkeley.edu>
2012-07-31 17:49:39 -0700

    changed pdifCone to accept min and max angles, added bimodal function

--- sim.py
+++ sim.py
@@ -65,24 +65,26 @@ def probKS( N, D ) :
     print n, sum
   
 # choose first point randomly on surface of sphere; keep choosing second point
-#   randomly until it lies within theta of first point; compute position angles
-#   pa1 and pa2 of these 2 vectors projected on the plane of the sky; compute
-#   angular difference between them; then reorder and generate the cumulative
-#   distribution function
-# thetaDegrees is half angle of cone around first point
+#   randomly until it lies between thetaMin and thetaMax of first point;
+#   compute position angles  pa1 and pa2 of these 2 vectors projected on the 
+#   plane of the sky; compute angular difference between them; then reorder
+#   and generate the cumulative distribution function
+# thetaMin is minimum angle between vector1 and vector2
+# thetaMax is maximum angle between vector1 and vector2
 # ntrials is the number of trials
 # nbins is the number of bins for the cumulative distribution function
 #   (it could be as large as ntrials but then wip couldn't plot it - wip
 #   will handle at most 20000 points)
 
-def paDifCone( thetaDegrees, outfile, ntrials=100000, nbins=1000 ) :
+def paDifCone( thetaMin, thetaMax, outfile, ntrials=100000, nbins=1000 ) :
   nsamps = 0
   pdif = numpy.zeros( ntrials, dtype=float ) 
-  costheta = math.cos( math.pi*thetaDegrees/180. )
+  costhetaMin = math.cos( math.pi*thetaMin/180. )
+  costhetaMax = math.cos( math.pi*thetaMax/180. )
   for n in range( 0, ntrials ) :
     [pa1,x1,y1,z1] = paRandom()
     cosphi = -1.01	# impossible value
-    while cosphi < costheta :
+    while (cosphi > costhetaMin) or (cosphi < costhetaMax) :
       [pa2,x2,y2,z2] = paRandom()
       cosphi = x1*x2 + y1*y2 + z1*z2	# yeah, this should be dot(a,b)
       nsamps = nsamps + 1
@@ -98,7 +100,31 @@ def paDifCone( thetaDegrees, outfile, ntrials=100000, nbins=1000 ) :
   for n in range( 0, ntrials, nskip ) :
     fout.write("%10.3f  %10.3f\n" % (pdifSorted[n], float(n)/float(ntrials) ) )
   fout.write("%10.3f  %10.3f\n" % (pdifSorted[ntrials-1], 1.0 ) )
+  return pdifSorted
+
+# bimodal distribution: fraction f1 lie between thetaMin1 and thetaMax2, franction (1-f1) 
+#   between thetaMin2 and thetaMax2
+
+def bimodal( f1, thetaMin1, thetaMax1, thetaMin2, thetaMax2, outfile, ntrials=100000, nbins=1000) :
+  ntrials1 = f1 * ntrials
+  ntrials2 = ntrials - ntrials1
+  pdif1 = paDifCone( thetaMin1, thetaMax1, 'dummy1', ntrials=ntrials1, nbins=nbins ) 
+  pdif2 = paDifCone( thetaMin2, thetaMax2, 'dummy2', ntrials=ntrials2, nbins=nbins ) 
+  pdifSorted = numpy.union1d(pdif1,pdif2)
+  nskip = ntrials/nbins
+  if nskip == 0 :
+    nskip = ntrials
+  fout = open( outfile, "w" )
+  for n in range( 0, ntrials, nskip ) :
+    fout.write("%10.3f  %10.3f\n" % (pdifSorted[n], float(n)/float(ntrials) ) )
+  fout.write("%10.3f  %10.3f\n" % (pdifSorted[ntrials-1], 1.0 ) )
+   
+  
+
+#  paDifCone( 180, "paDif180.dat" )		# outflows randomly oriented w/ respect to field
+#  paDifCone( 20, "paDif20.dat" )		# outflows within 20 degrees of field direction
+#  paDifCone2( 70, 110, "paDif90.dat" )   # outflows perpendicular to field
+#  paDifCone2( 0, 20, "paDif20B.dat" )   
 
 def examples( ) :
-  paDifCone( 180, "paDif180.dat" )		# outflows randomly oriented w/ respect to field
-  paDifCone( 20, "paDif20.dat" )		# outflows within 20 degrees of field direction
+  bimodal( 0.5, 0., 15., 75., 105., "bimodal15", ntrials=100000, nbins=1000)
