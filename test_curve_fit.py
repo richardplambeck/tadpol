9dec64027e9641bedb245e55962902d7c8e91948
Dick Plambeck <plambeck@astro.berkeley.edu>
2013-06-19 16:52:11 -0700

    added plotting routine

--- test_curve_fit.py
+++ test_curve_fit.py
@@ -2,11 +2,12 @@ import scipy.optimize
 import numpy
 import math
 import random
+import pylab
 
-PA0in = math.pi * 20./180.
+PA0in = math.pi * 70./180.
 P0in = 3.5*.05
 RMin = 5.e5
-QUnoise = 0.1
+QUnoise = 0.02
 
 freqlist = numpy.array([210., 211., 212, 213., 227., 228., 229., 230.])
 
@@ -15,7 +16,7 @@ x = (.30*.30)/(freqlist*freqlist) - (.30*.30)/(220.*220.)
   # lambdasq array, units m^2
 PAin = PA0in + RMin *  x 
    # noiseless PA array
-print 180./math.pi * PAin
+#print 180./math.pi * PAin
 
 if QUnoise > 1.e-7 :
   qr = numpy.random.normal(loc=0.,scale=QUnoise,size=len(freqlist))
@@ -32,9 +33,9 @@ ydata = numpy.concatenate( (Q,U) )
 xdata = numpy.concatenate( (x,x) )
 
 def func( xdata, p0, pa0, rm ) :
-  for n in range(0, len(xdata)/2) :
-    qmodel = p0 * numpy.cos(2 * (pa0 + rm*x))
-    umodel = p0 * numpy.sin(2 * (pa0 + rm*x))
+  xloc = xdata[0: len(xdata)/2.]
+  qmodel = p0 * numpy.cos(2 * (pa0 + rm*xloc))
+  umodel = p0 * numpy.sin(2 * (pa0 + rm*xloc))
   return numpy.concatenate( (qmodel,umodel) )
 
 print ""
@@ -56,4 +57,26 @@ if popt[0] < 0. :
 print "p0 = %.3f (%.3f)" % (popt[0], math.sqrt(pcov[0][0]))
 print "pa0 = %.2f (%.2f)" % (popt[1] * 180./math.pi, math.sqrt(pcov[1][1])*180./math.pi)
 print "RM = %.3e (%.3e)" % (popt[2], math.sqrt(pcov[2][2]))
-print pcov
+
+
+def plotfit( freqlist, freq0, Q, U, p0, pa0, rm ) :
+  pylab.ion()
+  fig = pylab.subplot(1,1,1)
+  frange = freqlist.max() - freqlist.min()
+  fmin = freqlist.min() - .05 * frange
+  fmax = freqlist.max() + .05 * frange
+  Ymax = abs(1.05 * numpy.concatenate( (Q,U,-1.*Q,-1*U) ).max())
+  fig.axis( [fmin, fmax, -1.*Ymax, Ymax] )
+  fig.grid( True )
+  fig.plot( freqlist, Q, 'ro')
+  fig.plot( freqlist, U, 'bo')
+  fx = numpy.arange(fmin,fmax,.1)
+  xp = (.30*.30)/(fx*fx) - (.30*.30)/(freq0*freq0) 
+  data = func( numpy.concatenate( (xp,xp) ), p0, pa0, rm)
+  Qfit = data[0:len(data)/2]
+  Ufit = data[len(data)/2 :] 
+  fig.plot( fx, Qfit, 'r-' )
+  fig.plot( fx, Ufit, 'b-' )
+  pylab.show()
+
+plotfit( freqlist, 220., Q, U, P0in, PA0in, RMin)
