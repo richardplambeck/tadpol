6b6240877a8cec7bdd740bac0e0771269682787f
Dick Plambeck <plambeck@astro.berkeley.edu>
2013-06-19 20:08:16 -0700

    added PA and RM fitting routine and plotting routine

--- RM.py
+++ RM.py
@@ -10,6 +10,8 @@ import shlex
 import string
 import leakSolve
 import paPlot
+import scipy.optimize
+import pylab
 
      
 # Extract Stokes (I,Q,U, or V) amplitude and rms from one line of uvflux output 
@@ -59,24 +61,16 @@ def makeStrList( LkFile ) :
   return strList
 
 
-# Generate table of I,Q,U,V in file vis["fileName"]
-# Uses SAVED leakages in files of "leakName.chxxx-yyy"
+# Generate table of I,Q,U,V vs channel range in file vis["fileName"], using leakages from file LkFile
+def makeIQUspectrum( vis, outfile, LkFile ) :
+  frqlist = []
+  Stokes = []
+  strList = makeStrList( LkFile )				# make list of available channel ranges in LkFile
+  leakSolve.makeFtable( vis )					# fill in the chfreq and chwidth items in vis dictionary
 
-def makeIQUtable( vis, outfile, LkFile ) :
-  strList = makeStrList( LkFile )
-  leakSolve.makeFtable( vis )
-
-  #leakSolve.makeFtable( vis2 )
-  #print " "
-  #print "LEAK: " + numpy.array_str( numpy.array(vis1["chfreq"])[0:5], precision=3, max_line_width=200 ) + " .... " \
-  #   + numpy.array_str( numpy.array(vis1["chfreq"])[-5:], precision=3, max_line_width=200 )
-  #print "DATA: " + numpy.array_str( numpy.array(vis2["chfreq"])[0:5], precision=3, max_line_width=200 ) + " .... " \
-  #   + numpy.array_str( numpy.array(vis2["chfreq"])[-5:], precision=3, max_line_width=200 )
-  #print " "
-
-  fout = open( outfile, "w" )
-  fout.write("# leaks from %s, data from %s\n" % ( LkFile, vis["fileName"] ) )
-  fout.write("#      f1     f2     I     rmsI     Q     rmsQ      U     rmsU     V    rmsV\n" )
+  fout = open( outfile, "a" )
+  fout.write("# Lkfile: %s   VisFile: %s,  selectStr: %s\n" % (LkFile, vis["fileName"], vis["selectStr"]) )
+  fout.write("#  f1       f2           I        rmsI           Q        rmsQ           U        rmsU           V        rmsV\n" )
   fout.close()
 
   for lineStr in strList :
@@ -93,6 +87,80 @@ def makeIQUtable( vis, outfile, LkFile ) :
     fout.write("%8.3f %8.3f   %10.3e %10.3e   %10.3e %10.3e   %10.3e %10.3e   %10.3e %10.3e\n" \
          % (fstart,fstop,I,rmsI,Q,rmsQ,U,rmsU,V,rmsV) )
     fout.close()
+    frqlist.append( 0.5*(fstart + fstop) )
+    Stokes.append( [I, rmsI, Q, rmsQ, U, rmsU] )
+
+  frq = numpy.array(frqlist)
+  freq0 = 226.
+  Ia = numpy.array(Stokes).transpose()[0]
+  Iavg = numpy.average( Ia )      # could be weighted
+  Qa = numpy.array(Stokes).transpose()[2]
+  rmsQa = numpy.array(Stokes).transpose()[3]
+  Ua = numpy.array(Stokes).transpose()[4]
+  rmsUa = numpy.array(Stokes).transpose()[5]
+  [p0, p0rms, pa0, pa0rms, rm, rmrms] = fitPARM( frq, freq0, Qa, Ua, rmsQa, rmsUa )
+  fout = open( outfile, "a" )
+  fout.write("# results: poli = %.3f (%.3f), PA = %.2f (%.2f),  RM = %.3e (%.3e)\n" \
+       % (p0, p0rms, pa0, pa0rms, rm, rmrms) )
+  fout.close()
+  plotfit( frq, freq0, Qa, Ua, p0, math.pi*pa0/180., rm )
+  
+# model Stokes Q, U as a function of 1/lambda^2
+# ... x = c^2/freq^2 - c^2/freq0^2, in m^2; each x given twice, once for Q, once for U
+# ... p0 is polarized intensity in Jy; a real number
+# ... pa0 is the position angle at the reference wavelength x=0, in radians; real number
+# ... rm is the rotation measure in radians/m^2; real number
+
+def func( x, p0, pa0, rm ) :
+  xhalf = x[0 : len(x)/2]
+  qmodel = p0 * numpy.cos(2 * (pa0 + rm*xhalf))
+  umodel = p0 * numpy.sin(2 * (pa0 + rm*xhalf))
+  return numpy.concatenate( (qmodel,umodel) )
+  
+
+# least squares fit of Q,U vs freq to get PA and RM
+def fitPARM( freq, freq0, Q, U, rmsQ, rmsU ) :
+  xhalf = (.30*.30)/(freq*freq) - (.30*.30)/(freq0*freq0)   # lambdasq - lambdasq0, m^2
+  x = numpy.concatenate( (xhalf,xhalf) )
+  y = numpy.concatenate( (Q,U) )
+  sigma = numpy.concatenate( (rmsQ,rmsU) )
+  popt,pcov = scipy.optimize.curve_fit( func, x, y, p0=[0.,0.,0.], sigma=sigma  )
+  if popt[0] < 0. :                 # if fit amplitude is negative, add 180 to 2 * PA
+    popt[0] = -1. * popt[0]
+    popt[1] = popt[1] + math.pi/2.
+  p0,pa0,rm = popt
+  pa0 = pa0 * 180./math.pi
+  try :
+    p0rms = math.sqrt(pcov[0][0])
+    pa0rms = math.sqrt(pcov[1][1]) * 180./math.pi
+    rmrms = math.sqrt(pcov[2][2]) 
+  except :
+    p0rms = pa0rms = rmrms = 0.
+  print "p0 = %.3f (%.3f)" % (p0, p0rms)
+  print "pa0 = %.2f (%.2f)" % (pa0, pa0rms)
+  print "RM = %.3e (%.3e)" % (rm, rmrms)
+  return [p0, p0rms, pa0, pa0rms, rm, rmrms]
+
+def plotfit( freqlist, freq0, Q, U, p0, pa0, rm ) :
+  pylab.clf()
+  pylab.ion()
+  fig = pylab.subplot(1,1,1)
+  frange = freqlist.max() - freqlist.min()
+  fmin = freqlist.min() - .05 * frange
+  fmax = freqlist.max() + .05 * frange
+  Ymax = abs(1.05 * numpy.concatenate( (Q,U,-1.*Q,-1*U) ).max())
+  fig.axis( [fmin, fmax, -1.*Ymax, Ymax] )
+  fig.grid( True )
+  fig.plot( freqlist, Q, 'ro')
+  fig.plot( freqlist, U, 'bo')
+  fx = numpy.arange(fmin,fmax,.1)
+  xp = (.30*.30)/(fx*fx) - (.30*.30)/(freq0*freq0)
+  data = func( numpy.concatenate( (xp,xp) ), p0, pa0, rm)
+  Qfit = data[0:len(data)/2]
+  Ufit = data[len(data)/2 :]
+  fig.plot( fx, Qfit, 'r-' )
+  fig.plot( fx, Ufit, 'b-' )
+  pylab.show()
 
   
 def RMsearch( infile, outfile ) :
@@ -126,52 +194,13 @@ def RMsearch( infile, outfile ) :
     print "RM = %.3e, P = %5.3f" % (RM,P)
     fout.write( "%.3e  %11.4e\n" % ( RM, P ) )
   fout.close()
-  
-# ==== deprecated below this line ==== #  
-
-# Make table of I,Q,U,V in file vis2["fileName"], using leakages from file vis1["fileName"]
-def makeTable( outfile ) :
-  leakSolve.makeFtable( vis1 )
-  leakSolve.makeFtable( vis2 )
-  print " =============== LEAK ============= "
-  print numpy.array_str( numpy.array(vis1["chfreq"]), precision=3, max_line_width=200 )
-  print " =============== DATA ============= "
-  print numpy.array_str( numpy.array(vis2["chfreq"]), precision=3, max_line_width=200 )
-
-  fout = open("outfile", "w")
-  fout.write("# leaks from %s, data from %s\n" % ( vis1["fileName"], vis2["fileName"] ) )
-  fout.write("#      f1     f2     I     rmsI     Q     rmsQ      U     rmsU     V    rmsV\n" )
-  fout.close()
-
-  for nwin in range (0,8) :
-    for ch1 in range( 47*nwin+3,47*nwin+40,7) :
-
-    # Solve for leakage
-      ch2 = ch1 + vis2["avgchan"] - 1
-      lineStrLeak = "chan,1,%d,%d" % ( ch1, vis2["avgchan"] )
-      fstartLeak = vis1["chfreq"][ch1-1] - 0.5*vis1["chwidth"][ch1-1]
-      fstopLeak = vis1["chfreq"][ch2-1] + 0.5*vis1["chwidth"][ch2-1]
-      leakSolve.delHd( vis1 )
-      leakSolve.addLeak( vis1, ch1 )
-      gpcopyLeak( vis1["fileName"], vis2["fileName"] )
-      saveLeak( vis1["fileName"], "leakC.ch%03d-%03d" % (ch1,ch2) )
-
-      selectString = "source(M81),-ant(7)"
-    # this is because Doppler tracking screwed everything up
-      if (nwin < 4) :
-        ch1 = ch1+1
-        ch2 = ch2+1
-      else :
-        ch1 = ch1-1
-        ch2 = ch2-1
-      lineStr = "chan,1,%d,%d" % ( ch1, vis2["avgchan"] )
-      fstart = vis2["chfreq"][ch1-1] - 0.5*vis2["chwidth"][ch1-1]
-      fstop = vis2["chfreq"][ch2-1] + 0.5*vis2["chwidth"][ch2-1]
-      print "Leak:  %s   %8.3f %8.3f" % (lineStrLeak, fstartLeak, fstopLeak)
-      print "Data:  %s   %8.3f %8.3f" % (lineStr, fstart, fstop)
-      [ I, rmsI, Q, rmsQ, U, rmsU, V, rmsV ] = getStokes2( vis2["fileName"], selectString, lineStr )
-      fout = open("M81resultsC", "a")
-      fout.write("%8.3f %8.3f   %10.3e %10.3e   %10.3e %10.3e   %10.3e %10.3e   %10.3e %10.3e\n" \
-         % (fstart,fstop,I,rmsI,Q,rmsQ,U,rmsU,V,rmsV) )
-      fout.close()
+   
+def test() :
+  frq =  numpy.array([ 220.57, 219.07, 223.82, 223.27, 232.08, 233.57, 228.82, 229.369] )
+  freq0 = 226.
+  Qa = numpy.array( [.0953, .0935, .106, .0938, .0869, .08834, .07027, .09538] )
+  Ua = numpy.array( [-.1177, -.1342, -.1321, -.1308,-.1305,-.1489,-.1456,-.1379] )
+  rmsQa = rmsUa = numpy.array( [0.,0.,0.,0.,0.,0.,0.,0.] )
+  [p0, p0rms, pa0, pa0rms, rm, rmrms] = fitPARM( frq, freq0, Qa, Ua, rmsQa, rmsUa )
+  plotfit( frq, freq0, Qa, Ua, p0, math.pi*pa0/180., rm )
 
