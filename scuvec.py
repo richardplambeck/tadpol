02a7a10f4dd6772d8bbded5f49cc2260c39bd364
Dick Plambeck <plambeck@astro.berkeley.edu>
2013-07-26 14:21:45 -0700

    this version includes Sharp routines

--- scuvec.py
+++ scuvec.py
@@ -7,31 +7,39 @@ srcname = "Serpens"
 radius = 10.
 
 def scuba() :
-  scale = 2.   # 2% pol = 1 arcsec on plot
+  scale = 1.5   # 1% pol = 2 arcsec on plot
   fin = open("table6.txt", "r")
   fout = open( "SCUBAOlay", "w" )
+  fout2 = open( "SCUBA.wip", "w" )
   nline = 0
   for line in fin:
     nline = nline + 1
     # if line.startswith("Serpens") :
     if True :
-      print line
       RA = line[33:45]
       DEC = line[45:57]
       polpct = float(line[77:82])
+
+      # quickie way of plotting all vectors the same length
+      # polpct = 4
+      # quickie way of plotting all vectors the same length
+
       PA = float(line[86:92])
       PAerr = float(line[92:])
       if PAerr < 15. :
         PA = PA + 90.   # rotate to show B-field direction
-        print "RA =",RA 
-        print "DEC =",DEC
-        print "polpct = %.1f" % polpct
-        print "PA = %.1f\n" % PA
-        #fout.write("ocircle hms dms %d no %s %s %.1f\n" % (nline,RA,DEC,radius) )
+        print "RA,DEC,polpct,PA,PAerr = %s %s %.1f %.1f %.1f" % (RA,DEC,polpct,PA,PAerr)
         [ra1,dec1,ra2,dec2] = vec(RA,DEC,polpct,PA,scale)
         fout.write("line hms dms %d no %s %s %s %s\n" % (nline,ra1,dec1,ra2,dec2) )
+        if (PAerr > 10.) :
+          fout2.write("lwidth 1\n")
+        fout2.write("radec %s %s\n" % (ra1,dec1))
+        fout2.write("drawradec %s %s\n" % (ra2,dec2))
+        if (PAerr > 10.) :
+          fout2.write("lwidth 5\n")
   fin.close()
   fout.close()
+  fout2.close()
 
 # vec computes ra and dec of line segment ends for pol vector
 # scale is arcsec per 1% pol
@@ -41,7 +49,7 @@ def vec(RA,DEC,polpct,PA,scale) :
   rhr = float(a[0])
   rmin = float(a[1])
   rsec = float(a[2])  
-  print rhr, rmin, rsec
+  #print rhr, rmin, rsec
   a = DEC.split()
   ddeg = float(a[0])
   dmin = float(a[1])
@@ -133,6 +141,95 @@ def hertz() :
       fout.write("line hms dms %d no %s %s %s %s\n" % (nline,ra1,dec1,ra2,dec2) )
   f1.close()
     
+
+# special routine to process SHARP data from Davidsen et al table 1
+def L1527() :
+  scale = 3.   # 1% pol corresponds to 4 arcsec on map
+  rhr = 4.
+  rmin = 39.
+  rsec = 53.9
+  ddeg = 26.
+  dmin = 3.
+  dsec = 11.
+  raSrc = math.pi * (rhr + rmin/60. + rsec/3600.)/12.
+  decSrc = math.pi * (ddeg + dmin/60. + dsec/3600.)/180.
+  
+  nline = 0
+  f1 = open("/fringe2/plambeck/pol/Hertz/L1527SHARP.dat")
+  fout = open("SHARPOlay", "w")
+  for line in f1:
+    a = line.split()
+    print a
+    draArcsec = float(a[0])
+    ddecArcsec = float(a[1])
+    polpct = float(a[2])
+    PA = float(a[4])
+    PAerr = float(a[5])
+    if PAerr < 15. :
+      decRadians = decSrc + math.pi * ddecArcsec/(180.*3600.) 
+      raRadians = raSrc + math.pi * draArcsec / (math.cos(decRadians) * 180.*3600.)
+      RA = hms(raRadians)
+      DEC = dms(decRadians)
+      PA = PA + 90.   # rotate to show B-field direction
+      print "RA =",RA 
+      print "DEC =",DEC
+      print "polpct = %.1f" % polpct
+      print "PA = %.1f\n" % PA
+      #fout.write("ocircle hms dms %d no %s %s %.1f\n" % (nline,RA,DEC,radius) )
+      [ra1,dec1,ra2,dec2] = vec(RA,DEC,polpct,PA,scale)
+      fout.write("line hms dms %d no %s %s %s %s\n" % (nline,ra1,dec1,ra2,dec2) )
+  f1.close()
+  fout.close()
     
+# generate arcmin grid, staggering az or el
+
+def gridMake( delArcmin, staggerEl=False ) :
+  fout = open("grid", "w")
+  deltaAz = delArcmin
+  deltaEl = 0.866 * delArcmin
+  if staggerEl :
+    deltaAz = 0.866 * delArcmin
+    deltaEl = delArcmin 
+  for m in range( -10, 11, 1 ) :
+    for n in range( -10, 11, 1 ) :
+      off = 0.
+      daz = n * deltaAz
+      dele = m * deltaEl
+      if staggerEl and (n % 2 == 1) :
+        dele = dele + 0.5 * deltaEl
+      elif (m % 2 == 1) :
+        print "staggering this row"
+        daz = daz + 0.5 * deltaAz
+      print daz, dele
+      fout.write("%9.3f %9.3f\n" % (daz,dele)  )
+  fout.close()
+
+
+
+def gridPlot( ) :
+  ramax = math.pi * (18. + 30./60. + 0.7/3600.)/12.
+  ramin = math.pi * (18. + 29./60. + 55.7/3600.)/12.
+  decmin = math.pi * ( 1. + 12./60. + 50./3600.)/180.
+  decmax = math.pi * ( 1. + 14./60. + 40./3600.)/180.
+  print ramin,ramax,decmin,decmax
+  ra0 = (ramax + ramin) / 2.
+  dec0 = (decmin + decmax) / 2.
+  print "center: ",hms(ra0),dms(dec0)
+  fin = open("grid", "r")
+  fout = open("gridOlay", "w")
+  fout2 = open("gridList", "w")
+  for line in fin :
+    a = line.split()
+    ra = ra0 + (math.pi * float(a[0]) /60 /12./15.) / math.cos(dec0)
+    dec = dec0 + math.pi*float(a[1])/60./180.
+    print a[0],a[1],hms(ra),dms(dec) 
+    if (ra >= ramin) and (ra <= ramax) and (dec >= decmin) and (dec <= decmax) :
+      print ">>",a[0],a[1],ra,dec
+      fout.write("ocircle hms dms no no %s %s 15.\n" % (hms(ra),dms(dec))) 
+      fout2.write("%s" % line)
+  fin.close()
+  fout.close()
+  fout2.close()
  
 	
+
