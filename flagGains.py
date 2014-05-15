faa6538756e1be131b88b8f5dad8b54dda55e18e
Dick Plambeck <plambeck@astro.berkeley.edu>
2014-05-15 11:19:22 -0700

    major rewrite to handle mfcal gains as well as selfcal gains

--- flagGains.py
+++ flagGains.py
@@ -4,18 +4,43 @@
 import numpy
 import math
 import vlbiCal
+import subprocess
+import shlex
 
-def flag( infile, cutoff ) :
-  [time, gainComplex] = vlbiCal.getGains( infile ) 
-  gainMag = numpy.abs( gainComplex )
-  for t,g in zip( time, gainMag ) :
-    if g.max() > cutoff :
-      s = "[ "
-      for n in range(0,15) :
-        if g[n] > cutoff : s = s + "*"
-        else : s = s + " "
-      s = s + "]"
-      print t, s 
+def getGains( visFile ) :
+  '''produce time and gain arrays'''
+  print "dump gain ratio from file %s" % visFile
+  g = []         # accumulate gains in 1-D list of floats
+  tt = []
+  dayno = []
+  type = None
+  p= subprocess.Popen( ( shlex.split('gpplt vis=%s log=tmpGain' % visFile ) ), \
+     stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.STDOUT)
+  result = p.communicate()[0]
+  print result
+  fin = open( 'tmpGain', 'r' )
+  for line in fin :
+    if line.startswith( '# Listing of the Amp of the gains for I' ) :
+      type = "I"
+    if line.startswith( '# Listing of the Amp of the gains for X,Y' ) :
+      type = "XY"
+    if (len(line) > 0) and (not line.startswith("#")) :
+      a = line.split()
+      if len(a) == 8 :  # begins new record
+        dayno.append( int(a[0]) )
+        tt.append( a[1] )
+        del a[0:2]      # discard daynumber and UT
+      for i in range(0,len(a)) :
+        g.append( float( a[i] ) )
+  fin.close()
+  #if type == "XY" :
+  #  gg = numpy.reshape( numpy.array(g), [len(tt),-1, 2] )     # gg[time, [ant, [GR,GL]]]
+  gg = numpy.reshape( numpy.array(g), [len(tt),-1] )     # gg[times][ants]
+  if type == "I" :
+    ggg = numpy.delete( gg, numpy.s_[15:], axis=1 )        # trim off gains for ants > 15
+  else :
+    ggg = numpy.delete( gg, numpy.s_[30:], axis=1 )        # trim off gains for ants > 15
+  return [tt, ggg]
 
 # turns "hh:mm:ss" into "hh:mm:ss,HH:MM:SS" where t2 = t1 + 1 sec
 def timeString( tstr ) :
@@ -49,14 +74,25 @@ def timeString( tstr ) :
   return "time(%s,%s)" % (t1str, t2str) 
 
 def makeFile ( infile, cutoff ) :
-  [time, gainComplex] = vlbiCal.getGains( infile ) 
+  [t, g ] = getGains( infile ) 
+  type = "I"
+  if len(numpy.shape(g)) > 2 :
+    type = "XY"
   fout = open( "flag.csh", "w" )
   fout.write ("#!/bin/csh\n\n")
   fout.write ("set FILE = %s\n\n" % infile)
-  gainMag = numpy.abs( gainComplex )
-  for t,g in zip( time, gainMag ) :
-    if g.max() > cutoff :
-      t1 = t2 = t
+  for tt,gg in zip( t, g) :
+    gstr = ""
+    for n,ggg in enumerate(gg) :
+      if (len(gg) > 15) and (n % 2 == 0) :
+        gstr = gstr + " "
+      gstr = gstr + "%5.1f" % ggg
+    bad = " "
+    if gg.max() > cutoff :
+      bad = "*"
+    
+      t1 = t2 = tt
       fout.write("uvflag flagval=flag vis=$FILE select='%s'\n" % (timeString(t1) ) )
-      fout.write("   # %s\n" % (numpy.array_str( g, precision=2, max_line_width=200) ) )
+      fout.write("   # %s\n" % gstr )
+    print "%s %s %s" % (tt,bad,gstr)
   fout.close()
