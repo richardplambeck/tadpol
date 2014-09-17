1b5ae61b36e01ac1082ee3439f35a09984b32e09
Dick Plambeck <plambeck@astro.berkeley.edu>
2014-09-16 20:45:10 -0700

    added annotation of spectral lines

--- ori.py
+++ ori.py
@@ -55,7 +55,7 @@ class spec:
         print q
         x.append( q[0] )
         y.append( q[1] )
-    p.plot( x, y, color="k" ) 
+    p.plot( x, y, color="r", linewidth=0.5 )
 
   def yminmax(self) :
     ymax = -1000.
@@ -64,7 +64,7 @@ class spec:
       if q[1] > ymax : ymax = q[1]
       if q[1] < ymin : ymin = q[1]
     diff = ymax - ymin
-    return [ ymin - .05*diff, ymax + .05*diff ]
+    return [ ymin - .05*diff, ymax + .05*diff + .3 ]
 
   def hist(self, fmin=0., fmax=0.) :
     pyplot.ioff()
@@ -84,7 +84,7 @@ class spec:
     for n in range(0,npanels) :
       np = np + 1    
       p = pyplot.subplot(3,1,np)
-      p.grid( True )
+      p.grid( True, linewidth=0.1, color="0.05" )   # color=0.1 is a light gray
       fmin = self.s[ ch1[n] ][ 0 ] 
       fmax = self.s[ ch1[n] + 1279 ][ 0 ] 
       x_formatter = matplotlib.ticker.ScalarFormatter(useOffset=False)
@@ -92,7 +92,37 @@ class spec:
       p.tick_params( axis='both', which='major', labelsize=6 )
       p.axis( [fmin, fmax, ymin, ymax] )
       self.panel( p, fstart=fmin, fstop=fmax )
+      self.ann( p, "linelist", fmin, fmax, ymax )
       if np % 3 == 0 :
         pyplot.savefig( pp, format='pdf', bbox_inches='tight' )
         np = 0
     pp.close()
+
+  #def ann(self,p) :
+  #  p.annotate("SO2 300K", xy=(649.81,-0.1), xytext=(649.81,.4), horizontalalignment="center", rotation="vertical", size=4, \
+  #     arrowprops=dict( arrowstyle="->", linewidth=0.2 ) )
+
+  def yvalue( self, fGHz ) :
+    '''find amplitude of spectrum at freq fGHz - use for annotating lines'''
+    ftest = self.s[0][0]
+    yvalue = self.s[0][1]
+    for q in self.s :
+      if (q[0] < fGHz) and (q[0] > ftest)  : 
+        ftest = q[0]
+        yvalue = q[1]
+    return yvalue
+    
+  def ann( self, p, linelistFile, fmin, fmax, ymax ) :
+    fin = open( linelistFile, "r" )
+    for line in fin :
+      if (len(line) > 1) and (not line.startswith("#")) :
+        a = line.split()
+        linefreq = float(a[0])
+        species = a[3]
+        if (linefreq >= fmin) and (linefreq <= fmax) :
+          yval = self.yvalue( linefreq )
+          p.annotate( species, xy=(linefreq,yval), xytext=(linefreq,ymax), horizontalalignment="center", rotation="vertical", size=4, \
+             arrowprops=dict( arrowstyle="->", linewidth=0.2 ) )
+
+       
+
