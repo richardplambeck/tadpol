6a90bfa3f276d0d843ede520b53456c875af0142
Dick Plambeck <plambeck@astro.berkeley.edu>
2015-06-16 18:20:37 -0700

    doit5 searches for best fit to sapphire waveplate data

--- pol.py
+++ pol.py
@@ -1562,3 +1562,84 @@ def doit4( nrefrac, LOGHz=220., angIdeg=45., tcm=1. ) :
     computedtanDelta = clight*computedAlpha/(LOGHz * 2. * math.pi * nrefrac)
     fout.write("  %8.5f  %6.1f %6.1f %6.1f  %7.3f %7.3f %7.3f   %7.5f\n" % (tanDelta, P3avg, P4avg, P5avg, T, R, L, computedtanDelta) )
   fout.close()
+
+# reads file produced by pb.calcT
+def readDataFile( dataFile ) :
+  fin = open( dataFile, "r" )
+  fdat = []
+  T3dat = []
+  T4dat = []
+  T5dat = []
+  for line in fin :
+    if not line.startswith('#') :
+      a = line.split()
+      fdat.append( float(a[0]) )
+      T3dat.append( float(a[1]) )
+      T4dat.append( float(a[2]) )
+      T5dat.append( float(a[3]) )
+  fin.close() 
+  return [ numpy.array(fdat), numpy.array(T3dat), numpy.array(T4dat), numpy.array(T5dat) ]
+   
+    
+# doit5 tries to FIT the data
+def doit5( dataFile, angIdeg=45., thetaDeg=0., tanDelta=1.e-4, dI=1., dtheta=1. ) :
+  Thot = 295
+  Tcold = 77
+  varsave = 1.e6
+  LOGHz = float( dataFile.split("_")[0] )
+  print "LOGHz = %0f" % LOGHz
+  fdat, T3dat, T4dat, T5dat = readDataFile( dataFile )
+  print T3dat
+  
+  for theta in numpy.arange( thetaDeg-5., thetaDeg+5+dtheta, dtheta ) :
+    for angI in numpy.arange( angIdeg-5., angIdeg+5.+dI, dI ) :
+      T3,T4,T5 = Tfit( LOGHz, fdat, angI, theta, tanDelta )  
+      variance = numpy.mean( pow( T3-T3dat, 2. )) + \
+         numpy.mean( pow( T4-T4dat, 2. ))
+      print "theta = %.1f, angI = %.1f, variance = %.1f" % (theta, angI, variance)
+      if variance < varsave :
+        angIbest = angI
+        thetabest = theta
+        varsave = variance
+  print "lowest variance for theta = %.1f, angI = %.1f" % (thetabest, angIbest)
+  T3,T4,T5 = Tfit( LOGHz, fdat, angIbest, thetabest, tanDelta )  
+  print len(fdat),len(T3)
+  plt.ioff()
+  fig = plt.subplot(1,1,1)
+  fig.plot( fdat, T3, color='red' )
+  fig.plot( fdat, T3dat, color='red' )
+  fig.plot( fdat, T4, color='blue' )
+  fig.plot( fdat, T4dat, color='blue' )
+  fig.plot( fdat, T5, color='purple' )
+  fig.plot( fdat, T5dat, color='purple' )
+  fig.grid( True )
+  plt.show( )
+
+
+def Tfit( LOGHz, fdat, angI, theta, tanDelta ) :
+  Thot = 295.
+  Tcold = 77.
+  npar = neff( theta )
+  nperp = neff( theta + 90. )
+  T3 = []
+  T4 = []
+  T5 = []
+  for fIF in fdat : 
+    TparLSB,TperpLSB,RparLSB,RperpLSB = plateTrans( LOGHz-fIF, npar, nperp, angIdeg=angI, tanDelta=tanDelta )
+    TparUSB,TperpUSB,RparUSB,RperpUSB = plateTrans( LOGHz+fIF, npar, nperp, angIdeg=angI, tanDelta=tanDelta )
+    if ( LOGHz < 200. ) :    # single polarization perp to plane of incidence
+      trans = (TperpLSB*TperpLSB.conjugate() \
+         + TperpUSB*TperpUSB.conjugate())/2.
+      refl = ( RperpLSB*RperpLSB.conjugate() \
+         + RperpUSB*RperpUSB.conjugate())/2.
+    else :                   # circular pol
+      trans = (TparLSB*TparLSB.conjugate() + TperpLSB*TperpLSB.conjugate() \
+         + TparUSB*TparUSB.conjugate() + TperpUSB*TperpUSB.conjugate())/4.
+      refl = (RparLSB*RparLSB.conjugate() + RperpLSB*RperpLSB.conjugate() \
+         + RparUSB*RparUSB.conjugate() + RperpUSB*RperpUSB.conjugate())/4.
+    loss = 1.-trans-refl
+    T3.append( trans*Thot + refl*Tcold + loss*Thot )
+    T4.append( trans*Tcold + refl*Thot + loss*Thot )
+    T5.append( trans*Tcold + refl*Tcold + loss*Thot )
+  return [ numpy.array(T3,dtype=float), numpy.array(T4,dtype=float), numpy.array(T5,dtype=float ) ]
+
