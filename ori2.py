7ec48ac29ff7ec575094e61f4f98c82eda2c7960
Dick Plambeck <plambeck@astro.berkeley.edu>
2015-10-13 19:18:17 -0700

    added routines to process splatalogue.csv, improved plots

--- ori2.py
+++ ori2.py
@@ -16,6 +16,7 @@ import matplotlib
 import matplotlib.pyplot as pyplot
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.patches import Circle
+from scipy import signal
 
 # dump out spectrum of selected region with imspec, return [freqLSR, flux]
 def getspec( infile, region='relpix,box(-2,-2,0,0)', vsource=5., hann=3, tmpfile="junk" ):
@@ -46,9 +47,11 @@ def getspec( infile, region='relpix,box(-2,-2,0,0)', vsource=5., hann=3, tmpfile
     p= subprocess.Popen( ( shlex.split("imspec in=%s region=%s options=list,eformat,noheader,hanning,%d log=%s" % \
       (infile,region,hann,tmpfile) )), stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.STDOUT)
     time.sleep(1)
-      # allow 1 sec to finish writing tmpfile
+    result = p.communicate()[0]
+      # allow 5 sec to finish writing tmpfile
     fin = open( tmpfile, "r" )
     for line in fin :
+      print line
       a = line.split()
       if len(a) > 2 :
         nchan = int( a[0] )
@@ -64,8 +67,10 @@ def getspec( infile, region='relpix,box(-2,-2,0,0)', vsource=5., hann=3, tmpfile
     print "read in %d lines" % len(freq)
     spectrum = numpy.array(sorted(zip(freq,flux)))
       # this sorts the freq,flux pairs in freq order
-    return numpy.split( spectrum, 2, axis=1 )
+    a,b = numpy.split( spectrum, 2, axis=1 )
       # this returns separate freq and flux arrays
+    return numpy.reshape(a,len(a)), numpy.reshape(b,len(b))
+      # unless I do this the array is officially a 2D array?!
 
 def yminmax( yarray ) :
     ymin = yarray.min()
@@ -83,29 +88,43 @@ def yvalue( freq, flux, fline ) :
 # annotates using linelistFile
 # linelistFile contains freq, species
 def ann( p, linelistFile, freq, flux, fmin, fmax, ymax ) :
-    fin = open( linelistFile, "r" )
-    for line in fin :
-      if (len(line) > 1) and (not line.startswith("#")) :
-        a = line.split(":")
-        print a[2], a[0]
-        linefreq = float(a[2])
-        species = a[0]
-        if (linefreq >= fmin) and (linefreq <= fmax) :
-          yval = yvalue( freq, flux, linefreq )
-          p.annotate( species, xy=(linefreq,yval), xytext=(linefreq,ymax), horizontalalignment="center", rotation="vertical", size=8, \
+    name,linefreq,TL,intensity = processLineList( linelistFile )
+    for n in range(0,len(name)) :
+        if (linefreq[n] >= fmin) and (linefreq[n] <= fmax) :
+          yval = yvalue( freq, flux, linefreq[n] )
+          p.annotate( "%s %.0fK %.3f" % (name[n],TL[n],intensity[n]), xy=(linefreq[n],yval), \
+             xytext=(linefreq[n],0.95*ymax), horizontalalignment="center", rotation="vertical", size=8, \
              arrowprops=dict( arrowstyle="->", linewidth=0.2 ) )
 
 
-def hist( freq, flux, plotParams, linelistFile=None ) :
-    #pyplot.ioff()
-    #pp = PdfPages("spectrum.pdf")
+# slide a template line profile along the spectrum to locate possible spectral lines
+# return convolution of freq with lineshape
+def findLines( fluxArray, continuum ) :
+    shape = numpy.zeros(160)
+    for n in range( 40,120 ) : 
+      shape[n] = 1.
+    # shape = signal.gaussian( 100, 10.)
+    # return signal.deconvolve( fluxArray-continuum, shape)
+    return numpy.convolve( fluxArray-continuum, shape, mode='same')
 
+def hist( freq, flux, plotParams, linelistFile=None ) :
+    if plotParams["pdf"] :
+      pyplot.ioff()
+      pp = PdfPages("spectrum.pdf")
     ymin = plotParams["ymin"]
     ymax = plotParams["ymax"]
     if ymin == ymax :
       ymin,ymax = yminmax( flux )
+      ymax = 1.2*ymax
     print "using ymin,ymax = ",ymin,ymax
-   
+
+  # locate spectral lines, find plot limits
+    cnv = findLines( flux, 0.5 )
+    cmin,cmax = yminmax( cnv )
+    print cmin,cmax
+    cmin = -cmax
+    cmax = 1.2 * cmax
+ 
     npanels = plotParams["npanels"]
     maxPanelsPerPage = plotParams["maxPanelsPerPage"]
     nlap = plotParams["nlap"]
@@ -118,35 +137,96 @@ def hist( freq, flux, plotParams, linelistFile=None ) :
     for npanel in range(0,npanels) :
       np = npanel % maxPanelsPerPage + 1
       p = pyplot.subplot(maxPanelsPerPage,1,np)
+      p2 = p.twinx()
+        # a twin of Axes sharing the same x-axis
       p.grid( True, linewidth=0.1, color="0.05" )   # color=0.1 is a light gray
       fmin = freq[nch1]
       fmax = freq[nch2]
       delta = fmax - fmin
       fmin = fmin - .04*delta
       fmax = fmax + .04*delta
+      p.axis( [fmin, fmax, ymin, ymax] )
+      p2.axis( [fmin, fmax, cmin, cmax] )
+      p2.plot( freq[nch1:nch2], cnv[nch1:nch2], color="b", linewidth=0.2 )
       x_formatter = matplotlib.ticker.ScalarFormatter(useOffset=False)
       p.xaxis.set_major_formatter( x_formatter )
       p.tick_params( axis='both', which='major', labelsize=8 )
-      p.axis( [fmin, fmax, ymin, ymax] )
       p.plot( freq[nch1:nch2], flux[nch1:nch2], color="r", linewidth=0.5 )
+      p.axhline( y=0.52, linestyle='--', color='blue')
       ann( p, plotParams["linelistFile"], freq, flux, fmin, fmax, ymax )
       if (np == maxPanelsPerPage) :
-        #pyplot.savefig( pp, format='pdf', bbox_inches='tight' )
+        if plotParams["pdf"] :
+          #pyplot.savefig( pp, format='pdf', bbox_inches='tight' )
+          pyplot.savefig( pp, format='pdf' )
         pyplot.show()
       nch1 = nch1 + nincr
       nch2 = nch2 + nincr
     if (np != maxPanelsPerPage ) :
-      #pyplot.savefig( pp, format='pdf', bbox_inches='tight' )
+      if plotParams["pdf"] :
+        # pyplot.savefig( pp, format='pdf', bbox_inches='tight' )
+        pyplot.savefig( pp, format='pdf' )
       pyplot.show()
-    #pp.close()
+    if plotParams["pdf"] :
+      pp.close()
 
-plotParams = { "npanels" : 1,
+plotParams = { "npanels" : 3,
                "maxPanelsPerPage" : 1,
                "nlap" : 100,
                "ymin" : -.2,
                "ymax" : 2.,
-               "linelistFile" : "LineList.csv" }
+               "linelistFile" : "splatalogue.csv", 
+               "pdf" : True }
 
-def doit( infile, region='relpix,box(-2,-2,0,0)', plotParams=plotParams ) :
-   [freq, flux] = getspec( infile, region )
+def doit( infile, region='relpix,box(-2,-2,0,0)', vsource=5.0, plotParams=plotParams ) :
+# def doit( infile, region='relpix,box(151,197,153,199)', plotParams=plotParams ) :
+# def doit( infile, region='relpix,box(-30,0,-10,20)', vsource=6., plotParams=plotParams ) :
+# def doit( infile, plotParams=plotParams ) :
+   [freq, flux] = getspec( infile, region=region, vsource=vsource )
    hist( freq, flux, plotParams )
+
+# check definitions of line intensities
+# Sij is line strength in Debyes (esu-cm)
+# EupperK is energy level of upper level (Kelvin)
+# ElowerK is energy level of lower level (Kelvin)
+# checked against data for SO+ line at 348115 MHz:
+# ... ori2.JPLintensity( 348115., 7.3489, 70.21, 53.51, 624., 300.) gives 0.00773027020636 -2.11180532535
+# ...   which seems to match JPL intensity in splatalogue
+def JPLintensity( freqMHz, Sba, EupperK, ElowerK, Qrs, T ) :
+    # h = 6.626e-27   # cm^2 g/sec
+    # c = 2.9979e10   # cm/sec
+    # Iba = 8.* pow(math.pi, 3.)/(3.*h*c) * freqMHz * Sba * (math.exp(-EupperK/T) - math.exp(-ElowerK/T))/ Qrs
+    Iba = 4.16231e-5 * freqMHz * Sba * (math.exp(-ElowerK/T) - math.exp(-EupperK/T))/ Qrs
+    print Iba, math.log10(Iba)
+
+def processLineList( lineListFile ) :
+    name = []
+    freq = []
+    TL = []
+    intensity = []
+    fin = open( lineListFile, "r" )
+    for line in fin :
+      if len(line) > 0 :
+        a = line.split(":")
+        if a[0] == "#Species" :
+          nameCol = 0
+          for n in range(1,len(a)) :
+            if a[n] == "Freq-GHz" :
+              freqCol = n
+            if a[n] == "E_L (K)" :
+              TLcol = n
+            if a[n] == "CDMS/JPL Intensity" :
+			  intensityCol = n
+            if a[n] == "Meas Freq-GHz" :
+			  altFreqCol = n
+          print "nameCol = %d, freqCol = %d, TLcol = %d, intensityCol = %d" % (nameCol,freqCol, TLcol,intensityCol)
+        elif not line.startswith("#") :
+          name.append( a[nameCol] )
+          if len(a[freqCol]) > 0 :
+            freq.append( float(a[freqCol]) )
+          else :
+            freq.append( float(a[altFreqCol]) )
+          TL.append( float(a[TLcol]) )
+          intensity.append( float(a[intensityCol]) )
+          print "%15s %12.5f %5.0f %6.4f" % ( name[-1], freq[-1], TL[-1], intensity[-1] )  
+    fin.close()
+    return name,freq,TL,intensity
